7a42336757d4ee4b9e107c5471b16553fa094a17
gopichand-VDL <ravalurugopichand@gmail.com>
2026-05-03 22:19:27 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'village-library-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      });
+      return cached || network;
+    })
+  );
+});
+
+self.addEventListener('push', e => {
+  const data = e.data?.json() || { title: 'Village Library', body: 'New update!' };
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon-192.png',
+      badge: '/icon-192.png',
+    })
+  );
+});
